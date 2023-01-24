d089c3aaff2a6ad2ef9a1993ad34ceef7e5104f8
luukalindgren <luuka.lindgren@gmail.com>
2023-01-24 12:38:01 +0200

    Finishing touches, like code formatting etc.

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,26 +1,46 @@
-import React from 'react';
+import React from "react";
 import Emoji from "./Emoji";
-import { NumericFormat } from "react-number-format"
-import StarRatings from 'react-star-ratings';
-
+import { NumericFormat } from "react-number-format";
+import StarRatings from "react-star-ratings";
 
 const Product = (props) => (
-    <>
-        <div className='product-image'>
-            <div className='product-image-index'>
-                {/\d/.test(props.index) ? <p>{props.index}</p> : <Emoji symbol={props.index} label="index"/>}
-            </div>
-            <img src={"https://bakery-4ea18f31.digi.loikka.dev" + props.image} alt={props.name} />
-        </div>
-        <div className='product-description'>
-            <b>{props.name}</b>
-            {props.selected ? <Emoji symbol="✓" label="checkmark"/> : null}
-            <p>{props.description}</p>
-            <div className='product-rating'>
-                <StarRatings starRatedColor='black' rating={props.rating} starDimension='15px' starSpacing='0px' numberOfStars={5} name='rating'/>
-            </div>
-            <NumericFormat value={props.amount} displayType="text" suffix={'€'} decimalSeparator=',' thousandSeparator=' ' className='price'/>
-        </div>
-    </>
+  <>
+    <div className="product-image">
+      <div className="product-image-index">
+        {/\d/.test(props.index) ? (
+          <p>{props.index}</p>
+        ) : (
+          <Emoji symbol={props.index} label="index" />
+        )}
+      </div>
+      <img
+        src={"https://bakery-4ea18f31.digi.loikka.dev" + props.image}
+        alt={props.name}
+      />
+    </div>
+    <div className="product-description">
+      <b>{props.name}</b>
+      {props.selected ? <Emoji symbol="✓" label="checkmark" /> : null}
+      <p>{props.description}</p>
+      <div className="product-rating">
+        <StarRatings
+          starRatedColor="black"
+          rating={props.rating}
+          starDimension="15px"
+          starSpacing="0px"
+          numberOfStars={5}
+          name="rating"
+        />
+      </div>
+      <NumericFormat
+        value={props.amount}
+        displayType="text"
+        suffix={"€"}
+        decimalSeparator=","
+        thousandSeparator=" "
+        className="price"
+      />
+    </div>
+  </>
 );
 export default Product;
